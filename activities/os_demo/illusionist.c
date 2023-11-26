a53604ab8943b4aaf1bf4295d586610982717016
mnoureddine <nouredd2@users.noreply.github.com>
2023-11-26 07:28:29 -0500

    added first activity

---
+++ activities/os_demo/illusionist.c
@@ -0,0 +1,42 @@
+/*
+ * CSSE 332: Operating Systems.
+ *
+ * This file allocates space for a signle integer and
+ * then changes its value.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+
+int
+main(int argc, char *argv[])
+{
+	int *iptr;
+	int i;
+
+  srand(time(NULL));
+
+	/* let's allocate and print an integer */
+	iptr = malloc(sizeof(int));
+	if (!iptr) {
+		perror("Ooops, out of memory...\n");
+		exit(EXIT_FAILURE);
+	}
+	*iptr = rand();
+
+	printf("(%u): Allocated int at location %p\n", getpid(), iptr);
+
+	i = 0;
+	while(i < 10) {
+		*iptr = i++;
+		printf("(%u): Updated the pointer's value to %d\n", getpid(),
+		       *iptr);
+		sleep(1);
+	}
+
+	/* done */
+	free(iptr);
+}
