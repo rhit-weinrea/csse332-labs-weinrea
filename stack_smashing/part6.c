0f18b7dae1e560e51cd9bf880abd1ac697c0d23d
Mohammad Noureddine <nouredd2@users.noreply.github.com>
2024-07-29 00:30:00 -0400

    removed parts 6 and 7 from sm and relabled 8 to 6

--- stack_smashing/part6.c
+++ stack_smashing/part6.c
@@ -1,39 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// comment this out if you want a real challenge!
-static const char *cheater[] = {"/bin/sh", NULL};
+#include <assert.h>
 
 void
 vulnerable_fn(char *argument)
 {
-  char buff[32];
+  char buff[1024];
   strcpy(buff, argument);
+}
+
+unsigned int getranduint(void)
+{
+  FILE *f;
+  unsigned int r;
+
+  f = fopen("/dev/urandom", "rb");
+  assert(f);
+  fread(&r, sizeof r, 1, f);
+  fclose(f);
+
+  return r;
 }
 
 
 int
 _main(int argc, char **argv)
 {
-  char *tmp;
-  int len;
+  unsigned int ssize;
 
-  if (argc < 2 || argc > 3) {
+  if (argc != 2) {
     fprintf(stderr, "ERROR: Need an input argument!\n");
     exit(EXIT_FAILURE);
   }
 
-  tmp = argv[1];
-  if(argc == 3) {
-    len = strlen(argv[1]) + strlen(argv[2]) + 2;
-    tmp = malloc(len);
-    memcpy(tmp, argv[1], strlen(argv[1]));
-    tmp[strlen(argv[1])] = 0x20;
-    memcpy(tmp + strlen(argv[1]) + 1, argv[2], strlen(argv[2]));
-    tmp[len] = 0;
-  }
+  ssize = getranduint();
+  alloca(ssize & 0xFF);
 
-  vulnerable_fn(tmp);
+  vulnerable_fn(argv[1]);
   exit(0);
 }
