fb4b427018f3a12baf8cfa42c18b3d045de0c9d3
Abby Weinreb <weinrea@rose-hulman.edu>
2024-12-09 15:57:54 -0500

    lab2

--- simpleshell/simpleshell.c
+++ simpleshell/simpleshell.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 
 int main() {
@@ -27,7 +28,46 @@ int main() {
     if(len_1 == strlen(command)){
       printf("Command is '%s' with no arguments\n", parsed_command[0]);
       parsed_command[1] = NULL;
+      char commandNum = parsed_command[0][0];
+      char *ptr = command;
+      //my stuff
+      int bg;
+      if(strncmp(command, "BG", 2) == 0)
+      {
+	bg = 1;
+	parsed_command[0] += 2;
+      }
+      int count = 1;
+      if(isdigit(command[0]))
+      {
+	printf("Will generate '%c' commands '%s'\n", command[0], parsed_command[0]);
+	count = strtol(ptr, &ptr, 10);
+	printf("num '%d'\n", count);
+	parsed_command[0] += 1;
+      }
+      for(int i = 0; i < count; i++)
+      {
+      	int status = -1;
+      	pid_t pid;
+      	pid = fork();
+      	if(pid == 0)
+      	{
+		execl(parsed_command[0], parsed_command[0], NULL);
+      	} else{
+	
+		if(!bg)
+		{
+			waitpid(pid, NULL, 0);
+		}
+      }
+      
+      }
 
+      for(int i = 0; i < count; i++)
+      {
+	int status;
+	pid_t child_pid = wait(&status);
+      }
       // leave this here, do not change it
       if(!strcmp(parsed_command[0], "quit") ||
           !strcmp(parsed_command[0], "exit")) {
@@ -37,7 +77,19 @@ int main() {
       command[len_1] = '\0';
       parsed_command[1] = command + len_1 + 1;
       printf("Command is '%s' with argument '%s'\n", parsed_command[0], parsed_command[1]);
-
+      
+      //my stuff
+      int status = -1;
+      pid_t pid;
+      pid = fork();
+      if(pid == 0)
+      {
+	execl(parsed_command[0], parsed_command[0], parsed_command[1], NULL);
+      }
+      else
+      {
+        wait(&status);
+      } 
       // leave this here, do not change it
       if(!strcmp(parsed_command[0], "quit") ||
           !strcmp(parsed_command[0], "exit")) {
