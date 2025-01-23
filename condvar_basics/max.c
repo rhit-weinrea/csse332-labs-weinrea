013a0d3d60559cce1f13414aa10779694c49fcd4
rhit-weinrea <101138434+rhit-weinrea@users.noreply.github.com>
2025-01-23 16:47:25 -0500

    Update max.c

--- condvar_basics/max.c
+++ condvar_basics/max.c
@@ -2,18 +2,43 @@
 #include <pthread.h>
 #include <unistd.h>
 
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;  
+int num = 0;
+
 void *thread(void *arg)
 {
-	char *letter = (char *)arg;
+ char *letter = (char *)arg;
+
+ pthread_mutex_lock(&m);
+
+  while(num >= 3){
+    pthread_cond_wait(&c, &m);
+  }
+  
+
+  num++;
+
+  pthread_mutex_unlock(&m);
+
+
 	printf("%c wants to enter the critical section\n", *letter);
 
 	printf("%c is in the critical section\n", *letter);
 	sleep(1);
 	printf("%c has left the critical section\n", *letter);
 
+  pthread_mutex_lock(&m);
+  num--;
+
+  pthread_cond_broadcast(&c);
+
+  pthread_mutex_unlock(&m);
+
+
+
 	return NULL;
 }
-
 int
 main(int argc, char **argv)
 {
