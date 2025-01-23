04eb044211a2c9a5304459eccd0d278f39302fbb
rhit-weinrea <101138434+rhit-weinrea@users.noreply.github.com>
2025-01-23 16:44:12 -0500

    Update inorder.c

--- condvar_basics/inorder.c
+++ condvar_basics/inorder.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;  
+int cur = 1;
 
 void *thread(void *arg)
 {
+
+   printf("%d wants to enter the critical section\n", *num);
   int *num = (int *)arg;
-  printf("%d wants to enter the critical section\n", *num);
+  pthread_mutex_lock(&m);
+
+  while(*num != cur){
+    pthread_cond_wait(&c, &m);
+  }
+  cur++;
 
   printf("%d is finished with the critical section\n", *num);
 
+  pthread_cond_signal(&c);
+  pthread_mutex_unlock(&m);
+
   return NULL;
 }
-
 int
 main(int argc, char **argv)
 {
