1e82b849307d89a6552c3bfccc981216f1a58f41
rhit-weinrea <101138434+rhit-weinrea@users.noreply.github.com>
2025-01-28 13:28:21 -0500

    Update stoplight.c

--- sample_exams/exam2/stoplight.c
+++ sample_exams/exam2/stoplight.c
@@ -3,93 +3,11 @@
 #include<pthread.h>
 #include<unistd.h>
 
-/*
-
-Imagine we have an intersection with a stoplight.  Cars can drive
-north south or east west, but we don't want north south cars in the
-intersection at the same time as east west cars.  However, any number
-of northsouth cars can be in the intersection at the same time, as
-long as the stoplight is going that way.
-
-The rules for the car threads are simple:
-
-1.  Cars freely enter the intersection if it green in their direction.
-
-2.  If it is yellow or green in the other direction, they do not enter
-the intersection
-
-3.  If they are in the intersection and the stoplight turns yellow,
-they proceed as normal and exit the intersection at their normal speed
-
-This stoplight is represented by a thread.  It changes from
-
-1.  green northsouth
-2.  yellow
-3.  green eastwest
-4.  yellow
-
-...and then back to the top.
-
-The stoplight changes from green to yellow after a specific time,
-regardless of what the car situation is (i.e. it will change from ns
-green to yellow, even if there are many ns cars and no ew cars
-waiting).
-
-However, the stoplight is smart about being yellow.  The stoplight is
-yellow exactly long enough for all cars currently in the intersection
-to leave.  If there are no cars in the intersection it will display
-yellow and then display the new green right away.  If they are cars it
-will change exactly as soon as all current cars leave the
-intersection.  Because no car will enter the intersection when the
-stoplight is yellow, there is no darger of the number of cars in the
-intersection increasing when the stoplight is yellow.
-
-The stoplight starts by being gree for northsouth cars and then transition the
-yellow and to green for eastwest and so on.. (Note the print statement at line
-135)
 
 
-Add concurrency primitives to the given code to implement the behavior
-described.  You may modify both car and stoplight threads.
-
-Example output:
-
-green in northsouth direction
-eastwest car nearing intersection
-northsouth car nearing intersection
-northsouth car entering intersection
-northsouth car nearing intersection
-northsouth car entering intersection
-yellow
-northsouth car leaving intersection
-northsouth car leaving intersection
-green in eastwest direction
-eastwest car entering intersection
-eastwest car nearing intersection
-eastwest car entering intersection
-northsouth car nearing intersection
-yellow
-eastwest car leaving intersection
-eastwest car leaving intersection
-green in northsouth direction
-northsouth car entering intersection
-northsouth car nearing intersection
-northsouth car entering intersection
-eastwest car nearing intersection
-yellow
-northsouth car leaving intersection
-northsouth car leaving intersection
-green in eastwest direction
-eastwest car entering intersection
-yellow
-eastwest car leaving intersection
-green in northsouth direction
-
- */
-
-#define YELLOW 0;
-#define EW 1;
-#define NS 2;
+#define YELLOW 0
+#define EW 1
+#define NS 2
 
 int status;
 
@@ -200,7 +118,7 @@ int main()
 
   // make sure we display the initial stoplight state before cars start
   printf("green in northsouth direction\n");
-  state = NS;
+  status = NS;
   pthread_create(&tid[i++],NULL,stoplight,NULL);
 
 
