9e55bc4efa6e42a78245a86c85aa48e8bbdecd93
rhit-weinrea <101138434+rhit-weinrea@users.noreply.github.com>
2025-02-03 20:52:06 -0500

    Update defs.h

--- xv6-riscv/kernel/defs.h
+++ xv6-riscv/kernel/defs.h
@@ -107,6 +107,9 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 uint64          spoon(void*);
+uint64          thread_alloc(void*);
+uint64          thread_free(void*);
+
 
 // swtch.S
 void            swtch(struct context*, struct context*);
